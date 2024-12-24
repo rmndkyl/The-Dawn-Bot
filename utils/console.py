e213af8794a4555fbfa85fd08f93f11e3d9c3d18
Mandaa <99118244+rmndkyl@users.noreply.github.com>
2024-12-24 15:03:31 +0700

    Update console.py

--- utils/console.py
+++ utils/console.py
@@ -1,8 +1,6 @@
-import os
 import sys
-
 import urllib3
-from art import tprint
+
 from loguru import logger
 
 
@@ -16,17 +14,3 @@ def setup():
         "message}</white>",
     )
     logger.add("./logs/logs.log", rotation="1 day", retention="7 days")
-
-
-def show_dev_info():
-    os.system("cls")
-    tprint("JamBit")
-    print("\033[36m" + "Channel: " + "\033[34m" + "https://t.me/JamBitPY" + "\033[34m")
-    print(
-        "\033[36m"
-        + "GitHub: "
-        + "\033[34m"
-        + "https://github.com/Jaammerr"
-        + "\033[34m"
-    )
-    print()
